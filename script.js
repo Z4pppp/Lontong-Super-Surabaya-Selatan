394a39073c364028cf2afba595590b3968f71c45
Z4pppp <muhammadzafiar29@gmail.com>
2024-06-25 12:31:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle nav links for responsive design
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('active');
+});
